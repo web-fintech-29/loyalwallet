0856f0edc7fd34ded4e3a1fbaf0d07b6f9d10a3e
Nolo Space <nolo@darkstar.example.net>
2020-10-30 15:35:09 +0200

    adjusted database model and functions for frontend

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
-const db = mongoose.connect('mongodb://localhost/loyalwallet');
-const port = process.env.PORT || 3030;
-const loyalwalletUser = require('./models/userModel');
-
