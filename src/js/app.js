4422f7aae948994356f0a793e8270dd4366dccca
Nolo Space <nolo@darkstar.example.net>
2020-10-30 15:44:32 +0200

    login button and register button work

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,54 @@
 
 
+function register(){
+    const firstname = document.getElementById('firstName');
+    const lastname = document.getElementById('lastName');
+    const username = document.getElementById('username');
+    const emailaddress = document.getElementById('email-address');
+    const password = document.getElementById('create-password');
+    const confirmpassword = document.getElementById('password-confirm');
 
-/*const User =  {
-    "firstName": "Jason",
-    "lastName": "Watmore",
-    "username": "jason",
-    "password": "my-super-secret-password"
-}
-
-const options = {
-    method: 'post',
-    url: 'http://127.0.0.1:4000/users/register',
-    data: {
-        "firstName": "Jason",
-        "lastName": "Watmore",
-        "username": "jason",
-        "password": "my-super-secret-password"
-    },
-} */
-export function register(){
-    axios.post('http://127.0.0.1:4000/users/register')
-            .then(response => {
+    if (password.value === confirmpassword.value){
+        axios.post('http://127.0.0.1:4000/users/register',{
+            "firstName": firstname.value,
+            "lastName": lastname.value,
+            "username": username.value,
+            "emailAddress": emailaddress.value,
+            "password": password.value
+        },{
+            "Content-Type": "application/json"},)
+                .then(response => {
+                    firstname.value ='';
+                    lastname.value = '';
+                    username.value = '';
+                    emailaddress.value = '';
+                    password.value = '';
+                    confirmpassword.value = '';    
                     console.log(response);
-            }, (error) => {
-                    console.log(error)
-            });    
+                }, (error) => {
+                        console.log(error.response);
+                });    
+    }
 }
 
-/*document.getElementById('create_account')
-                .onclick = axios
-                            .post('http://localhost:4000/users/register', 
-                            {   "firstName": "Jason",
-                                "lastName": "Watmore",
-                                "username": "jason",
-                                "password": "my-super-secret-password"
-                            })
-                            .then(response => {
-                                console.log(response);
-                            }, (error) => {
-                                console.log(error)
-                            }); 
-*/
-//async function signIn(){
+
+function logIn(){
+    const emailAddress = document.getElementById('email-address');
+    const password = document.getElementById('password');
     
-//}
+    axios.post('http://127.0.0.1:4000/users/authenticate', { 
+                                                                "username": emailAddress.value,
+                                                                "password": password.value})
+        .then( response => {
+                emailAddress.value = "";
+                password.value = "";
+                console.log(response);
+        },
+            (error) => {
+                console.log(error.response);
+        });
+}
+
+
+
+
